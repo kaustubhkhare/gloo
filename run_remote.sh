646e3d20aa1f200c6c35393b5edc0b4a5d9e4944
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-11 13:26:09 -0600

    Run script

--- run_remote.sh
+++ run_remote.sh
@@ -1,7 +1,8 @@
-hosts=("pc281.emulab.net" "pc283.emulab.net" "pc306.emulab.net" "pc290.emulab.net" "pc288.emulab.net" "pc309.emulab.net" "pc295.emulab.net" "pc312.emulab.net" "pc305.emulab.net" "pc320.emulab.net" "pc308.emulab.net" "pc300.emulab.net")
-#hosts=("pc203.emulab.net" "pc201.emulab.net" "pc202.emulab.net" "pc204.emulab.net")
+#hosts=("pc281.emulab.net" "pc283.emulab.net" "pc306.emulab.net" "pc290.emulab.net" "pc288.emulab.net" "pc309.emulab.net" "pc295.emulab.net" "pc312.emulab.net" "pc305.emulab.net" "pc320.emulab.net" "pc308.emulab.net" "pc300.emulab.net")
+hosts=("pc203.emulab.net" "pc201.emulab.net" "pc202.emulab.net" "pc204.emulab.net")
 
-for host in "${hosts[@]}"
+for i in "${!hosts[@]}"
 do
-  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${host} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh"
+  echo "Running $i -> ${hosts[$i]}"
+  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[$i]} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh 4 ${i} enp9s4f0 treebroadcast" -t bash
 done
