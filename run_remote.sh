5324a4681e2b31b2f040830b65959f5d71d9b5ca
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-11 16:10:07 -0600

    Run script

--- run_remote.sh
+++ run_remote.sh
@@ -1,8 +1,11 @@
 #hosts=("pc281.emulab.net" "pc283.emulab.net" "pc306.emulab.net" "pc290.emulab.net" "pc288.emulab.net" "pc309.emulab.net" "pc295.emulab.net" "pc312.emulab.net" "pc305.emulab.net" "pc320.emulab.net" "pc308.emulab.net" "pc300.emulab.net")
 hosts=("pc203.emulab.net" "pc201.emulab.net" "pc202.emulab.net" "pc204.emulab.net")
 
+ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[0]} "rm /proj/UWMadison744-F21/groups/akc/rendezvous_checkpoint/*"
+
 for i in "${!hosts[@]}"
 do
   echo "Running $i -> ${hosts[$i]}"
-  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[$i]} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh 4 ${i} enp9s4f0 treebroadcast" -t bash
+  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[$i]} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh 4 ${i} enp9s4f0 treebroadcast"
 done
+wait
