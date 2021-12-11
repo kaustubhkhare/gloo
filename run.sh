daed1e55188f0e2612482bda431203e7559e49b5
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-11 12:26:05 -0600

    Run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-#mpirun -report-bindings -hostfile hosts -np 6 treebroadcast
-mpirun -report-bindings -rankfile rankfile -np 4 treebroadcast --by-node
+rm /proj/UWMadison744-F21/groups/akc/rendezvous_checkpoint/*
+cd /proj/UWMadison744-F21/groups/akc/gloo/build
+PREFIX=$4 SIZE=$1 RANK=$2 NETWORK=$3 $(find . -name $4)
