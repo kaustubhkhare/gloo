cbc2c85fe0532b756420cd79cf21c5f491ad6a3a
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-11 12:37:45 -0600

    Run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-mkdir -p logs
+mkdir -p ~/logs
 rm /proj/UWMadison744-F21/groups/akc/rendezvous_checkpoint/*
 cd /proj/UWMadison744-F21/groups/akc/gloo/build
 PREFIX=$4 SIZE=$1 RANK=$2 NETWORK=$3 $(find . -name $4) > ~/logs/${4}.log 2>&1 &
