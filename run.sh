1500d17c2a2b7f99bc81ee3b8f83400f99db01c7
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-13 00:27:06 -0600

    bug fix

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 mkdir -p ~/logs
 cd /proj/UWMadison744-F21/groups/akc/gloo/build
-iters=(30000 20000 13333 8888 5925 3950 2633 1500 1000)
-#iters=(1500 1000)
+#iters=(30000 20000 13333 8888 5925 3950 2633 1500 1000)
+iters=(5925 3950 2633 1500 1000)
 #iters=(1 1 1 1 1 1 1 1 1)
-cnt=(10 100 500 1000 5000 20000 80000 100000 140000)
-#cnt=(100000 140000)
+#cnt=(10 100 500 1000 5000 20000 80000 100000 140000)
+cnt=(5000 20000 80000 100000 140000)
 rm ~/logs/${4}.log
 #netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
 for i in `seq 0 6`; do
-  echo "Running: PREFIX=${4} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &> ~/logs/${4}.log &" >> ~/logs/${4}.log
+  echo "Running: PREFIX=${4}-${i} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &> ~/logs/${4}.log &" >> ~/logs/${4}.log
   PREFIX="${4}-${i}" SIZE=$1 RANK=$2 NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &>> ~/logs/${4}.log
   echo ""
   echo ""
