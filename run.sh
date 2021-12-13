23654067864f0983314e4af9c956fd3159e3c2bd
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-12 18:44:04 -0600

    framework code

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/logs
 cd /proj/UWMadison744-F21/groups/akc/gloo/build
-netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
-echo "Running: PREFIX=${4} SIZE=${1} RANK=${2} NETWORK=${netw} INPUT_SIZE=${5} ITERS=${6} $(find . -name $4) &> ~/logs/${4}.log &" > ~/logs/${4}.log
-PREFIX=$4 SIZE=$1 RANK=$2 NETWORK=${netw} INPUT_SIZE=$5 ITERS=${6} $(find . -name $4) &>> ~/logs/${4}.log &
+#netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
+echo "Running: PREFIX=${4} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${5} ITERS=${6} $(find . -name $4) &> ~/logs/${4}.log &" > ~/logs/${4}.log
+PREFIX=$4 SIZE=$1 RANK=$2 NETWORK=${3} INPUT_SIZE=$5 ITERS=${6} $(find . -name $4) &>> ~/logs/${4}.log &
 
 #PREFIX=test2 SIZE=4 RANK=0 NETWORK=enp9s4f0 ./treebroadcast
--- run_remote.sh
+++ run_remote.sh
@@ -1,11 +1,12 @@
 #hosts=("pc281.emulab.net" "pc283.emulab.net" "pc306.emulab.net" "pc290.emulab.net" "pc288.emulab.net" "pc309.emulab.net" "pc295.emulab.net" "pc312.emulab.net" "pc305.emulab.net" "pc320.emulab.net" "pc308.emulab.net" "pc300.emulab.net")
 hosts=("pc791.emulab.net" "pc793.emulab.net" "pc781.emulab.net"  "pc802.emulab.net"  "pc809.emulab.net"  "pc795.emulab.net"  "pc786.emulab.net"  "pc785.emulab.net"  "pc782.emulab.net"  "pc788.emulab.net"  "pc811.emulab.net"  "pc796.emulab.net")
+ifaces=("enp6s0f1" "enp6s0f1" "enp6s0f0" "enp6s0f0" "enp6s0f0" "enp4s0f0" "enp4s0f1" "enp4s0f1" "enp6s0f1" "enp4s0f1" "enp6s0f0" "enp4s0f0")
 #hosts=("pc203.emulab.net" "pc201.emulab.net" "pc202.emulab.net" "pc204.emulab.net")
 ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[0]} "rm /proj/UWMadison744-F21/groups/akc/rendezvous_checkpoint/*"
-#ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[0]} "cd /proj/UWMadison744-F21/groups/akc/gloo/ && git pull && cd /proj/UWMadison744-F21/groups/akc/gloo/build/ && make -j4"
+ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[0]} "cd /proj/UWMadison744-F21/groups/akc/gloo/ && git pull && cd /proj/UWMadison744-F21/groups/akc/gloo/build/ && make -j4"
 for i in "${!hosts[@]}"
 do
   echo "Running $i -> ${hosts[$i]}"
-  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[$i]} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh 16 ${i} enp6s0f0 treebroadcast 16 20"
+  ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[$i]} "cd /proj/UWMadison744-F21/groups/akc/gloo && bash run.sh 16 ${i} ${ifaces[$i]} treebroadcast 16 20"
 done
 wait
