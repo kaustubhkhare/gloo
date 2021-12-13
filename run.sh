cba9da18044fa5b336f3eb6d18997870f67e8229
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-12 20:12:13 -0600

    bug fix

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/logs
 cd /proj/UWMadison744-F21/groups/akc/gloo/build
 iters=(30000 20000 13333 8888 5925 3950 2633)
-cnt=(10 100 500 1000 5000 20000 80000)
+cnt=(16 128 512 1024 5000 20000 80000)
 rm ~/logs/${4}.log
 #netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
-for i in `seq 0 6`; do
+for i in `seq 0 3`; do
   echo "Running: PREFIX=${4} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &> ~/logs/${4}.log &" >> ~/logs/${4}.log
   PREFIX="${4}-${i}" SIZE=$1 RANK=$2 NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &>> ~/logs/${4}.log
   echo ""
