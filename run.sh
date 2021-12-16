8e326d157f8e0ff49be47e625cbdf17fb62bc872
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-15 23:15:09 -0600

    size

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/logs
 cd /proj/UWMadison744-F21/groups/akc/gloo/build
-iters=(30000 20000 13333 8888 5925 3950 2633 1500 1000)
+#iters=(30000 20000 13333 8888 5925 3950 2633 1500 1000)
 #iters=(1, 1)
 iters=(1 1 1 1 1 1 1 1 1)
-#cnt=(10 100 500 1000 5000 20000 80000 100000 140000)
+cnt=(10 100 500 1000 5000 20000 80000 100000 140000)
 #cnt=(2000000, 5000000)
 rm ~/logs/${4}.log
 #netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
