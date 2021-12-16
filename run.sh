cad5662cf6a2e026306a2dc41c2a56712b3665a9
kaustubhkhare <kharekaustubh10@gmail.com>
2021-12-16 00:16:58 -0600

    iters

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ cnt=(10 100 500 1000 5000 20000 80000 100000 140000)
 rm ~/logs/${4}.log
 #netw=$((ifconfig | grep -B1 $(ping -c1 $(hostname | cut -d "." -f1) | head -1 | cut -d " " -f3 | cut -c2- | rev | cut -c2- | rev)) | head -1 | cut -d " " -f1 | rev | cut -c2- | rev)
 for i in `seq 0 8`; do
-  echo "Running: PREFIX=${4}-${i} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &> ~/logs/${4}.log &" >> ~/logs/${4}.log
+  echo "Running: PREFIX=${4}-${i}${i} SIZE=${1} RANK=${2} NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &> ~/logs/${4}.log &" >> ~/logs/${4}.log
   PREFIX="${4}-${i}" SIZE=$1 RANK=$2 NETWORK=${3} INPUT_SIZE=${cnt[$i]} ITERS=${iters[$i]} $(find . -name $4) &>> ~/logs/${4}.log
   echo ""
   echo ""
