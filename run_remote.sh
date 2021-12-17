74c97fbb97a9adf9b1c34ebe32531794d9f46fc1
Abhinav <abhinavagarwal1996@gmail.com>
2021-12-17 17:15:14 -0600

    try to fix compile

--- run_remote.sh
+++ run_remote.sh
@@ -7,10 +7,10 @@ ifaces=("enp6s0f1" "enp6s0f1" "enp6s0f0" "enp6s0f0" "enp6s0f0" "enp4s0f0" "enp4s
 #hosts=("pc791.emulab.net" "pc793.emulab.net" "pc781.emulab.net"  "pc802.emulab.net"  "pc809.emulab.net"  "pc795.emulab.net"  "pc786.emulab.net"  "pc785.emulab.net"  "pc782.emulab.net" "pc811.emulab.net" "pc788.emulab.net"  "pc796.emulab.net")
 #ifaces=("enp6s0f1" "enp6s0f1" "enp6s0f0" "enp6s0f0" "enp6s0f0" "enp4s0f0" "enp4s0f1" "enp4s0f1" "enp6s0f1" "enp6s0f0" "enp4s0f1" "enp6s0f0")
 
-size=8
-lessthansize=7
+size=4
+lessthansize=3
 #binary=sagbroadcast
-binary=treereduce
+binary=sagreduce
 #checkpointdir=rendezvous_checkpoint
 checkpointdir=rendezvous_checkpoint-CT
 ssh -o StrictHostKeyChecking=no -p 22 -l kkhare ${hosts[0]} "rm /proj/UWMadison744-F21/groups/akc/${checkpointdir}/*"
